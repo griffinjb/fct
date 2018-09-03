efbaa3fa2687a3e55504ee8c8fe8e4e3d88be518
griffinjb <griffinj@oregonstate.edu>
2018-09-02 20:38:31 -0700

    lozenge

--- impIMG.py
+++ impIMG.py
@@ -5,35 +5,29 @@ import sys
 import random
 import json
 import numpy as np
+import scipy
+from scipy.fftpack import fft, fftfreq
+from scipy.io import wavfile
 
 def show(im):
 	cv2.imshow('image',im)
 	cv2.waitKey(1)
 
-def imin(fn):
+def pimin(fn):
+	im = cv2.imread(fn)
+	him = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
+	return(him)
+
+def imin(fn,thresh):
 
 	im = cv2.imread(fn)
 	
 	gim = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-	(t1,tim) = cv2.threshold(gim,127,255,cv2.THRESH_BINARY)
+	(t1,tim) = cv2.threshold(gim,thresh,255,cv2.THRESH_BINARY)
 
 	return(tim)
 	# cv2.imwrite('ds.jpg',tim)	
-
-def HellDriver(im):
-
-	hitlist = []
-
-	for x in range(0,im.shape[0]):
-		for y in range(0,im.shape[1]):
-			if im[x,y] != 0:
-				hits = []
-				queue = []
-				recursionHell(x,y,im,hits,queue)
-				hitlist.append(hits)
-
-	return(hitlist)
 
 def endlessDriver(im):
 	hitlist = []	
@@ -57,6 +51,19 @@ def endlessHell(xi,yi,im,hits,queue):
 	for xm,ym in [[0,-1],[0,1],[1,0],[-1,0]]:
 		queue.append([xi+xm,yi+ym])
 
+def HellDriver(im):
+
+	hitlist = []
+
+	for x in range(0,im.shape[0]):
+		for y in range(0,im.shape[1]):
+			if im[x,y] != 0:
+				hits = []
+				queue = []
+				recursionHell(x,y,im,hits,queue)
+				hitlist.append(hits)
+
+	return(hitlist)
 
 def recursionHell(xi,yi,im,hits):
 
@@ -84,16 +91,113 @@ def paint_it(patches,im):
 
 	return(im)
 
+def initObj(fn,pfn):
+	thresh = getThresh(cv2.imread(fn))
+	im = imin(fn,thresh)
+	palIM = pimin(pfn)
+
+	patches = endlessDriver(im)
+	# patches = HellDriver(im)
+	pim = paint_it(patches,im)
+	# show(pim)
+	# cv2.imwrite('coloredin.png',pim)
+	cpim = cv2.cvtColor(pim,cv2.COLOR_GRAY2BGR)
+	hpim = cv2.cvtColor(cpim,cv2.COLOR_BGR2HSV)
+	# cv2.imwrite('hsvim.png',cpim)
+	swamptreats = swampland(patches,hpim,palIM)
+	swamptreats.write('powa.png')
+	return(swamptreats)
+
+def randGen(swampy):
+	valCache = [0 for i in range(0,swampy.getLen())]
+	v2alCache = [0 for i in range(0,swampy.getLen())]
+
+	sins = np.sin(np.array([i for i in range(0,100)])/10)
+
+	for j in range(0,100):
+		print(j)
+		for i in range(0,swampy.getLen()):
+			if j%10 == 0:
+				v2alCache[i] = valCache[i]
+				valCache[i] = random.randint(0,255)
+			# swampy.modVAL(i,sins[j]*255)
+			swampy.modVAL(i,v2alCache[i] + (j%10)/10*(v2alCache[i]-valCache[i]))
+			# swampy.modVAL(i,random.randint(0,255))
+
+			# swampy.write('powa.png')
+		show(cv2.cvtColor(swampy.imOUT,cv2.COLOR_HSV2BGR))
+		movie.append(cv2.cvtColor(swampy.imOUT,cv2.COLOR_HSV2BGR))
+	return(movie)
+
+def impAudio(fn):
+	fs,aud = wavfile.read(fn)
+	chnCNT = len(aud.shape)
+	if chnCNT == 2:
+		aud = aud.sum(axis=1)/2
+	l_aud = aud.shape[0]
+	secs = l_aud/float(fs)
+	Ts = 1.0/fs
+	t = scipy.arange(0,secs,Ts)
+	FFT = abs(fft(aud))
+	FFT_side = FFT[range(l_aud/2)]
+	freqs = fftfreq(aud.size,t[1]-t[0])
+	fft_freqs = np.array(freqs)
+	freqs_side = freqs[range(l_aud/2)]
+	fft_freqs_side = np.array(freqs_side)
+
+def hsExtract(im):
+	hues = []
+	sats = []
+	for x in range(0,im.shape[0]):
+		for y in range(0,im.shape[1]):
+			hues.append(im[x,y,0])
+			sats.append(im[x,y,1])
+	return([hues,sats])
+
+def hueExtract(im):
+	hues = []
+	for x in range(0,im.shape[0]):
+		for y in range(0,im.shape[1]):
+			hues.append(im[x,y,0])
+	# hud = list(set(hues))
+	return hues
+
+def writeThatVideoShit(movie):
+	height,width,layers = movie[1].shape
+	fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	video = cv2.VideoWriter('video.avi',fourcc,20,(width,height))
+
+	for frame in movie:
+		video.write(frame)
+
+	cv2.destroyAllWindows()
+	video.release()
+
+def getThresh(im):
+	gim = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+	buf = 0
+	yo  = 0
+	while True:
+		buf = yo
+		yo = input()
+		if yo == 'q':
+			return(buf)
+		else: yo = int(yo)
+		(t1,tim) = cv2.threshold(gim,yo,255,cv2.THRESH_BINARY)
+		show(tim)
+
 class swampland:
-	def __init__(self,porg,hsvIMIN):
+	def __init__(self,porg,hsvIMIN,palIM):
 		self.coordsets = []
 		self.baseHSV_Sets = []
 
 		satBase = 255
 		valBase = 125
 		# HueList = [0,55,84,173,200,270,281,298,325,360]
-		HueList = [i for i in range(0,360)]
-		base_color_pool = [[Hue*179/360,satBase,valBase] for Hue in HueList]
+		# HueList = [i for i in range(0,360)]
+		# HueList = hueExtract(palIM)
+		hsList = hsExtract(palIM)
+		base_color_pool = [[hs[0],hs[1],valBase] for hs in zip(hsList[0],hsList[1])]
 
 		i = 0
 		for pog in porg:
@@ -130,81 +234,20 @@ class swampland:
 			for pix in self.coordsets[i]:
 				self.imOUT[pix[0],pix[1]] = self.baseHSV_Sets[i]
 
-
-def initObj(fn):
-	# sys.setrecursionlimit(40000)
-	im = imin(fn)
-	patches = endlessDriver(im)
-	# patches = HellDriver(im)
-	pim = paint_it(patches,im)
-	# show(pim)
-	# cv2.imwrite('coloredin.png',pim)
-	cpim = cv2.cvtColor(pim,cv2.COLOR_GRAY2BGR)
-	hpim = cv2.cvtColor(cpim,cv2.COLOR_BGR2HSV)
-	# cv2.imwrite('hsvim.png',cpim)
-	swamptreats = swampland(patches,hpim)
-	swamptreats.write('powa.png')
-	return(swamptreats)
-
-def randGen(swampy):
-	valCache = [0 for i in range(0,swampy.getLen())]
-	v2alCache = [0 for i in range(0,swampy.getLen())]
-
-	sins = np.sin(np.array([i for i in range(0,100)])/10)
-
-	for j in range(0,100):
-		print(j)
-		for i in range(0,swampy.getLen()):
-			if j%10 == 0:
-				v2alCache[i] = valCache[i]
-				valCache[i] = random.randint(0,255)
-			# swampy.modVAL(i,sins[j]*255)
-			swampy.modVAL(i,v2alCache[i] + (j%10)/10*(v2alCache[i]-valCache[i]))
-			# swampy.modVAL(i,random.randint(0,255))
-
-			# swampy.write('powa.png')
-		show(cv2.cvtColor(swampy.imOUT,cv2.COLOR_HSV2BGR))
-		movie.append(cv2.cvtColor(swampy.imOUT,cv2.COLOR_HSV2BGR))
-	return(movie)
-
-
-def impAudio(fn):
-	
-
-
 if __name__ == '__main__':
-
 	loadJ = False
 
 	fin = 'panda.jpg'
-	jout = 'sonicJet.json'
+	pfn = 'abs.png'
 	fout = 'lit.png'
+	ain = 'toffee.wav'
 
+	swampy = initObj(fin,pfn)
+	i = 0
+	movie = []
 
-	if not loadJ:
+	# aud = impAudio(ain)
 
-		swampy = initObj('panda.jpg')
-		i = 0
-		movie = []
+	movie = randGen(swampy)
 
-		aud = impAudio(fn)
-
-		# movie = randGen(swampy)
-
-		height,width,layers = movie[1].shape
-		fourcc = cv2.VideoWriter_fourcc(*'XVID')
-		video = cv2.VideoWriter('video.avi',fourcc,20,(width,height))
-
-		for frame in movie:
-			video.write(frame)
-
-		cv2.destroyAllWindows()
-		video.release()
-		# with open(jout,'wt') as fout:
-		# 	json.dump(movie, fout, sort_keys=True, indent=4, separators=(',',': '))
-
-	else:
-
-		with open(jout,'r') as fp:
-			movie = json.load(fp)
-			movie = np.array(movie)
+	writeThatVideoShit(movie)
